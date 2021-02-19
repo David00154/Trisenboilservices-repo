8098ce603b7930a4d0904ff511e4b199562a6320
David00154 <davidbriggs00154@gmail.com>
2021-02-19 22:49:17 +0100

    updated _server.js

--- src/_server.js
+++ src/_server.js
@@ -1,3 +1,14 @@
-const {app} =  require('../__sapper__/build/index');
+// const app =  require('../__sapper__/build/index');
 
-module.exports = app;
+// module.exports = app;
+
+const fs = require('fs');
+const path = require("path");
+
+if(fs.existsSync(path.resolve(process.cwd() + '/__sapper__'))) {
+    const app =  require('../__sapper__/build/index');
+    module.exports = app;
+    console.log("__sapper__ has been built and created")
+} else {
+    console.log("__sapper__ has not been generated")
+}
