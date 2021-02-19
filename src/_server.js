df25dc8287d879bbf2fa72cbbadf9875b1a26a23
David00154 <davidbriggs00154@gmail.com>
2021-02-19 22:54:01 +0100

    removed vercel.json

--- src/_server.js
+++ src/_server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const path = require("path");
 
 if(fs.existsSync(path.resolve(process.cwd() + '/__sapper__'))) {
-    const app =  require('../__sapper__/build/index');
+    const app = require('../__sapper__/build/index');
     module.exports = app;
     console.log("__sapper__ has been built and created")
 } else {
